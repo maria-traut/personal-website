038602cd65c165e132c25a99530f22c31189d493
Maria <mia.gotcha@proton.me>
2026-05-03 22:48:04 +0200

    implement react and start creating components

---
+++ personal-website/src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Navigation from "./components/Navigation";
+import Content from "./components/Content";
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <Navigation />
+      <Content />
+      <Footer />
+    </>
+  );
+}
---
+++ personal-website/src/components/Header.jsx
@@ -0,0 +1,10 @@
+import "./Header.css";
+
+export default function Header() {
+  return (
+    <header className="header">
+      <h1 className="header__heading">hi, i'm maria</h1>
+      <h2 className="header__subheading">web developer to be</h2>
+    </header>
+  );
+}
