a80f0d940a24ec63ba82565e879961869a0c3404
Maria <mia.gotcha@proton.me>
2026-05-06 11:52:30 +0200

    try fetching music api

--- personal-website/src/App.jsx
+++ personal-website/src/App.jsx
@@ -2,12 +2,14 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Navigation from "./components/Navigation";
 import Content from "./components/Content";
+import AudiusPlayer from "./components/AudiusPlayer";
 
 export default function App() {
   return (
     <>
       <Header />
       <Navigation />
+      <AudiusPlayer />
       <Content />
       <Footer />
     </>
--- personal-website/src/components/Header.jsx
+++ personal-website/src/components/Header.jsx
@@ -3,8 +3,10 @@ import "./Header.css";
 export default function Header() {
   return (
     <header className="header">
-      <h1 className="header__heading">hi, i'm maria</h1>
-      <h2 className="header__subheading">web developer to be</h2>
+      <h1 className="header__heading">hey</h1>
+      <h2 className="header__subheading">
+        welcome to my web development playground
+      </h2>
     </header>
   );
 }
